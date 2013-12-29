e4b8685ad3de294adc37b818eabf9c66e5d852a8
Anatoly Pashin <anatoly.pashin@gmail.com>
2013-12-30 02:24:59 +1100

    some docrine enhancements

--- lib/plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Record.php
+++ lib/plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Record.php
@@ -1064,6 +1064,8 @@ abstract class Doctrine_Record extends Doctrine_Record_Abstract implements Count
         } else {
             unset($this->_references[$name]);
         }
+
+        return $this;
     }
 
     /**
